5598088cbc0428694e463b584c94940f773db594
MatthewLeaman <leaman.30@osu.edu>
2024-12-11 01:01:11 -0500

    complete

--- server/models.py
+++ server/models.py
@@ -11,7 +11,24 @@ class Author(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators 
+    @validates('name')
+    def validate_name(self, key, name):
+        names = [author.name for author in Author.query.all()]
+        if name == '':
+            raise ValueError("name is invalid")
+        elif name in names:
+            raise ValueError("no")
+        return name
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        phone_numbers = [author.phone_number for author in Author.query.all()]
+        num = int(phone_number)
+        if len(phone_number) != 10:
+            raise ValueError("no")
+        elif type(num) is not int:
+            raise ValueError("no")
+        return phone_number
+    
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -27,8 +44,37 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators  
-
+    @validates('title')
+    def validates_title(self, key, title):
+        if title == '':
+            raise ValueError("no")
+        elif "Won't Believe" in title:
+            return title
+        elif "Secret" in title:
+            return title
+        elif "Top" in title:
+            return title
+        elif "Guess" in title:
+            return title
+        else:
+            raise ValueError("no")
+    
+    @validates('content')
+    def validates_content_length(self, key, content):
+        if len(content) < 250:
+            raise ValueError("no")
+        return content
+    
+    @validates('summary')
+    def validates_summary_length(self, key, summary):
+        if len(summary) > 250:
+            raise ValueError("no")
+        return summary
+    
+    @validates('category')
+    def validates_category(self, key, category):
+        if 'Fiction' not in category:
+            raise ValueError("no")
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
